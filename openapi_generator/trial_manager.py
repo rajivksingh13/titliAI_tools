bb92d6bfeb9bfd58e880dfd24a30678a05c559ee
rajivksingh13 <rajeksingh2@gmail.com>
2026-01-17 17:09:16 +0530

    Version 4 commit for OpenAPI Auto Gen Tool with 1 days Trial window

--- openapi_generator/trial_manager.py
+++ openapi_generator/trial_manager.py
@@ -16,7 +16,7 @@ class TrialManager:
     """Manages trial period for the application."""
     
     # Trial duration in days (change this to set the trial period)
-    TRIAL_DAYS = 7  # Default: 7 days
+    TRIAL_DAYS = 1  # Default: 7 days
     APP_NAME = "OpenAPI-AutoGen"
     
     def __init__(self):
